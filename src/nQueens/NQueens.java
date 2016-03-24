24e9140c04127c416cf103d4796b4d945defc45f
alu0100309553 <alu0100309553@ull.edu.es>
2016-03-24 10:23:45 +0000

    Primer commit

---
+++ src/nQueens/NQueens.java
@@ -0,0 +1,43 @@
+package nQueens;
+
+import java.util.ArrayList;
+
+import clock.Clock;
+
+public class NQueens {
+  private ArrayList<Integer> sol = new ArrayList<Integer>();
+  private int counter = 0;
+  private Clock timer = new Clock();
+  public void run(){
+    for (int i = 0; i<8; i++){
+      sol.add(i);
+    }
+    timer.start();
+    solve(0, new ArrayList<Integer>() , new ArrayList<Integer>(), new ArrayList<Integer>());
+  }
+  private void solve(int k, ArrayList<Integer> col, ArrayList<Integer> diag45, ArrayList<Integer> diag135){
+    if (k == 8){
+      timer.stop();
+      System.out.println(timer.elapsedTime());
+      System.out.println("" + ++counter + sol);
+    }
+    else{
+      for(int j = 1; j<=8; j++){
+        if(!col.contains(j) && !diag45.contains(j-k) && !diag135.contains(j+k)){
+          sol.set(k, j);
+          col.add(j);
+          diag45.add(j-k);
+          diag135.add(j+k);
+          solve(k+1, col, diag45, diag135);
+          col.remove(col.size()-1);
+          diag45.remove(diag45.size()-1);
+          diag135.remove(diag135.size()-1);
+        }
+      }
+    }
+  }
+  boolean alignedsolution(ArrayList<Integer> diag45, ArrayList<Integer> diag135){
+    return true;
+  }
+
+}
---
+++ src/nQueens/Test.java
@@ -0,0 +1,11 @@
+package nQueens;
+
+public class Test {
+
+  public static void main(String[] args) {
+    NQueens prueba = new NQueens();
+    prueba.run();
+
+  }
+
+}
